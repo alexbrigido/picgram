af517ce47ab65ef462195925746726fab98aa92c
ALEXANDRE DIAS BRIGIDO <alexbrigido@hotmail.com>
2021-07-06 20:32:30 -0300

    correcao register store

--- lib/app/modules/register/register_store.dart
+++ lib/app/modules/register/register_store.dart
@@ -1,5 +1,7 @@
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:picgram/app/constants.dart';
 import 'package:mobx/mobx.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 part 'register_store.g.dart';
 
@@ -7,7 +9,10 @@ class RegisterStore = _RegisterStoreBase with _$RegisterStore;
 abstract class _RegisterStoreBase with Store {
 
   FirebaseAuth _firebaseAuth;
-  _RegisterStoreBase(this._firebaseAuth);
+  SharedPreferences _sharedPreferences;
+  _RegisterStoreBase(this._firebaseAuth, this._sharedPreferences) {
+    _firebaseAuth.authStateChanges().listen(_onAuthChange);
+  }
 
   @observable
   User? user;
@@ -16,9 +21,22 @@ abstract class _RegisterStoreBase with Store {
   bool loading = false;
 
   @action
+  void _onAuthChange(User? user) {
+    if (user?.isAnonymous ?? true) { //elvis operator
+      this.user = null;
+    }else {
+      this.user = user;
+    }
+  }
+
+  @action
   Future<void> registerUser({required String name, required String email, required String password}) async {
     loading = true;
-    final credential = await _firebaseAuth.createUserWwithEmailAndPassword(email: email, password: password);
-    _firebaseAuth.signInWith
+    final credential = await _firebaseAuth.createUserWithEmailAndPassword(email: email, password: password);
+    _firebaseAuth.signInWithEmailAndPassword(email: email, password: password);
+    await credential.user?.updateDisplayName(name);
+    _sharedPreferences.setBool(Constants.SPK_REGISTER_DONE, true);
+    loading = false;
   }
+
 }
