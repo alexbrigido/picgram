dc214d9ee0b0969a60d61876ad9fbea652b72278
ALEXANDRE DIAS BRIGIDO <alexbrigido@hotmail.com>
2021-07-16 19:50:02 -0300

    add chat

--- lib/app/app_module.dart
+++ lib/app/app_module.dart
@@ -1,3 +1,4 @@
+import 'package:picgram/app/modules/chat/chat_store.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
@@ -16,7 +17,6 @@ import 'package:shared_preferences/shared_preferences.dart';
 import 'modules/home/home_module.dart';
 
 class AppModule extends Module {
-
   SharedPreferences _sharedPreferences;
   FirebaseApp _firebaseApp;
   AppModule(this._sharedPreferences, this._firebaseApp);
@@ -25,37 +25,45 @@ class AppModule extends Module {
 
   @override
   List<Bind> get binds => [
-    Bind.singleton((i) => _sharedPreferences),
-    Bind.instance(_firebaseApp),
-    Bind.factory((i) => FirebaseAuth.instance),
-    Bind.factory((i) => FirebaseFirestore.instance),
-    Bind.factory((i) => FirebaseStorage.instance),
-  ];
+        Bind.singleton((i) => _sharedPreferences),
+        Bind.instance(_firebaseApp),
+        Bind.factory((i) => FirebaseAuth.instance),
+        Bind.factory((i) => FirebaseFirestore.instance),
+        Bind.factory((i) => FirebaseStorage.instance),
+      ];
 
   @override
   List<ModularRoute> get routes => [
-    //Modular.initialRoute == /
-    ModuleRoute(Modular.initialRoute, module: _initialModule()),
-    ModuleRoute(Constants.Routes.ONBOARDING, module: OnboardingModule()),
-    ModuleRoute(Constants.Routes.REGISTER, module: RegisterModule(), transition: TransitionType.rightToLeftWithFade),
-    ModuleRoute(Constants.Routes.LOGIN, module: LoginModule()),
+        //Modular.initialRoute == /
+        ModuleRoute(Modular.initialRoute, module: _initialModule()),
+        ModuleRoute(Constants.Routes.ONBOARDING, module: OnboardingModule()),
+        ModuleRoute(Constants.Routes.REGISTER,
+            module: RegisterModule(),
+            transition: TransitionType.rightToLeftWithFade),
+        ModuleRoute(Constants.Routes.LOGIN, module: LoginModule()),
 
-    ModuleRoute(Constants.Routes.HOME, module: HomeModule(), guards: [_routeGuard]),
-    ModuleRoute(Constants.Routes.FEED, module: FeedModule(), guards: [_routeGuard]),
-    ModuleRoute(Constants.Routes.SEARCH, module: SearchModule(), guards: [_routeGuard]),
-    ModuleRoute(Constants.Routes.PROFILE, module: ProfileModule(), guards: [_routeGuard]),
-  ];
+        ModuleRoute(Constants.Routes.HOME,
+            module: HomeModule(), guards: [_routeGuard]),
+        ModuleRoute(Constants.Routes.FEED,
+            module: FeedModule(), guards: [_routeGuard]),
+        ModuleRoute(Constants.Routes.SEARCH,
+            module: SearchModule(), guards: [_routeGuard]),
+        ModuleRoute(Constants.Routes.PROFILE,
+            module: ProfileModule(), guards: [_routeGuard]),
+      ];
 
   Module _initialModule() {
-    final onboardingDone = _sharedPreferences.getBool(Constants.SPK_ONBOARDING_DONE) ?? false;
+    final onboardingDone =
+        _sharedPreferences.getBool(Constants.SPK_ONBOARDING_DONE) ?? false;
     if (onboardingDone) {
-      final registerDone = _sharedPreferences.getBool(Constants.SPK_REGISTER_DONE) ?? false;
+      final registerDone =
+          _sharedPreferences.getBool(Constants.SPK_REGISTER_DONE) ?? false;
       if (registerDone) {
         return LoginModule();
-      }else {
+      } else {
         return RegisterModule();
       }
-    }else {
+    } else {
       return OnboardingModule();
     }
   }
